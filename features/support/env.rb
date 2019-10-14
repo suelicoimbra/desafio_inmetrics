ae03dae9679afbd9a7e4f1cfcb702d61f2f76b5a
suelicoimbra <sueli.silvestre@yahoo.com.br>
2019-10-14 12:56:42 -0300

    adicionando arquivo github-ci.yml

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,22 +1,40 @@
-require 'cucumber'
-require 'rspec'
-require 'selenium-webdriver'
-require 'capybara'
-require 'capybara/cucumber'
-require 'site_prism'
-require 'pry'
+require "cucumber"
+require "rspec"
+require "selenium-webdriver"
+require "capybara"
+require "capybara/cucumber"
+require "site_prism"
+require "pry"
 
-
-#Configurando o driver Capybara
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
+CONFIG = YAML.load_file(File.join(Dir.pwd, "/cucumber.yaml"))
 
 #Setando a configuração do driver como padrão
 Capybara.default_driver = :selenium
 
-#timeout padrão na execução
-Capybara.default_max_wait_time = 15
+case ENV["BROWSER"]
+when "firefox"
+  @driver =:selenium
+when "chrome"
+  @driver =:selenium_chrome
+when "headless"
+   Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument "--headless"
+      options.add_argument "--disable-gpu"
+      options.add_argument "--no-sandbox"
+      options.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+  @driver =:selenium_chrome_headless
+else 
+  puts "Invalid browser"
+end
 
-#Maximizar a tela ao iniciar o teste
-Capybara.page.driver.browser.manage.window.maximize
+#configuraçoes
+Capybara.configure do |config|
+  config.default_driver = @driver
+  config.app_host = CONFIG["url"]
+  config.default_max_wait_time = 15
+end
+
